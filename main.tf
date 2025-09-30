3b8c592558f581d6560d5aacbf39dc837c45e311
tyler <tyler@embold.com>
2025-09-30 15:30:39 -0700

    :art: Update SSH setup icon

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "coder_script" "ssh_setup" {
   agent_id           = var.agent_id
   script             = templatefile("${path.module}/run.sh", { HOSTS_LINE = join(" ", var.hosts) })
   display_name       = "SSH Setup"
-  icon               = "/icon/ssh.svg"
+  icon               = "/emojis/1f511.png"
   run_on_start       = true
   start_blocks_login = true
 }
