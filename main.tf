24edd355a618e5e3767b5369a92281541bd4e16a
tyler <tyler@embold.com>
2026-02-02 18:34:32 -0800

    :bento: Update icons

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "coder_script" "ssh_setup" {
   agent_id           = var.agent_id
   script             = templatefile("${path.module}/run.sh", { HOSTS_LINE = join(" ", var.hosts) })
   display_name       = "SSH Setup"
-  icon               = "/emojis/1f511.png"
+  icon               = "https://api.embold.net/icons/?name=ssh-wordmark.svg&color=009dff"
   run_on_start       = true
   start_blocks_login = true
 }
